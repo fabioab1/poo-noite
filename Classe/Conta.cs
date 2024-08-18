e35bf7a9fab8bfff9df581dc45562385b6876f46
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2024-08-18 19:48:57 -0300

    Pasta da aula de introdução a classes em POO C#

---
+++ Classe/Conta.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classe
+{
+    public class Conta
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        
+    }
+}
---
+++ Classe/Program.cs
@@ -0,0 +1,28 @@
+﻿using Classe;
+
+Conta c1; // Declaração de uma variável
+
+c1 = new Conta(); // Instância do objeto é do new para frente
+c1.numero = 1;
+c1.titular = "Fatec";
+c1.saldo = 100;
+System.Console.WriteLine($"Número da conta: {c1.numero}\nTitular: {c1.titular}\nSaldo: {c1.saldo}");
+
+System.Console.WriteLine();
+
+Conta c2 = new Conta();
+c2.numero = 2;
+c2.titular = "Empresa Jr";
+c2.saldo = 200;
+System.Console.WriteLine($"Número da conta: {c2.numero}\nTitular: {c2.titular}\nSaldo: {c2.saldo}");
+
+System.Console.WriteLine();
+
+Conta c3 = new Conta();
+System.Console.Write("Cadastre o número da conta: ");
+c3.numero = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Insira o nome do titular da conta: ");
+c3.titular = Console.ReadLine();
+System.Console.WriteLine("Insira o saldo existente na conta: ");
+c3.saldo = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine($"Número da conta: {c3.numero}\nTitular: {c3.titular}\nSaldo: {c3.saldo}");
