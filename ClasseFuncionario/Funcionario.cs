8223fc48bbe2a725bfc340b019f52edcefa15af0
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2024-08-19 20:25:38 -0300

    Pasta para as classes Funcionario e Main (Program) criadas e lógica da classe Funcionario implementada.

---
+++ ClasseFuncionario/Funcionario.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClasseFuncionario
+{
+    public class Funcionario
+    {
+        public int codigo;
+        public string nome;
+        public double salario;
+
+        // Declaração dos métodos.
+        public void MostraAtributos()
+        {
+            System.Console.WriteLine($"Código: {codigo}\n" +
+            $"Nome: {nome}\n" +
+            $"Salário: R$ {salario.ToString("0.00")}.");
+        }
+
+        public void AumentarSalario(int porcentagem)
+        {
+            salario += salario * porcentagem / 100;
+        }
+
+        // Criar um método para aplicar uma porcentagem
+        // de aumento ao salário.
+    }
+}
---
+++ ClasseFuncionario/Program.cs
@@ -0,0 +1,35 @@
+﻿// Função main()
+
+using ClasseFuncionario;
+// Instanciar objeto para conseguir cadastrar algo.
+Funcionario f1 = new Funcionario();
+
+System.Console.Write("Digite o códgo: ");
+f1.codigo = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Digite o nome: ");
+f1.nome = Console.ReadLine();
+System.Console.Write("Digite o salário: ");
+f1.salario = Convert.ToDouble(Console.ReadLine());
+f1.MostraAtributos(); // Enviando uma mensagem.
+
+System.Console.WriteLine();
+
+Funcionario f2 = new Funcionario();
+
+System.Console.Write("Digite o códgo: ");
+f2.codigo = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Digite o nome: ");
+f2.nome = Console.ReadLine();
+System.Console.Write("Digite o salário: ");
+f2.salario = Convert.ToDouble(Console.ReadLine());
+f2.MostraAtributos(); // Enviando uma mensagem.
+
+System.Console.WriteLine();
+double somaSalarios = f1.salario + f2.salario;
+System.Console.WriteLine($"Soma dos salários: R$ {somaSalarios}");
+
+System.Console.WriteLine();
+System.Console.Write("Digite uma porcentagem de aumento: ");
+int porcentagemAumento = Convert.ToInt32(Console.ReadLine());
+f2.AumentarSalario(porcentagemAumento);
+f2.MostraAtributos();
